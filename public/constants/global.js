897eff5bc27b0634eca99632f5b594353f79950c
yunchu <yunchu@live.unc.edu>
2017-04-03 09:24:18 -0400

    fix

--- public/constants/global.js
+++ public/constants/global.js
@@ -2,6 +2,6 @@
  * Created by yunchu on 3/18/17.
  */
 module.exports = {
-    apihost: 'https://secure-peak-60366.herokuapp.com'
+    apihost: 'https://secret-beach-62203.herokuapp.com'
     // apihost: 'http://localhost:3002'
 }

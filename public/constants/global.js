b66ced61bd1ae649a09a3d1d7a80481bc3630fd3
yunchu <yunchu@live.unc.edu>
2017-04-03 08:47:40 -0400

    update

--- public/constants/global.js
+++ public/constants/global.js
@@ -2,6 +2,6 @@
  * Created by yunchu on 3/18/17.
  */
 module.exports = {
-    // apihost: 'https://secure-peak-60366.herokuapp.com'
-    apihost: 'http://localhost:3002'
+    apihost: 'https://secure-peak-60366.herokuapp.com'
+    // apihost: 'http://localhost:3002'
 }

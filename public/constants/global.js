889d21631825f5b91f897dce3dad555699a52589
yunchu <yunchu@live.unc.edu>
2017-04-16 16:25:16 -0400

    second sprint

--- public/constants/global.js
+++ public/constants/global.js
@@ -2,6 +2,6 @@
  * Created by yunchu on 3/18/17.
  */
 module.exports = {
-    apihost: 'https://secret-beach-62203.herokuapp.com'
-    // apihost: 'http://localhost:3002'
+    // apihost: 'https://secret-beach-62203.herokuapp.com'
+    apihost: 'http://localhost:3002'
 }
